9c93e8119daf44a0e66d8b2afc33fed03e23fd53
Rodrigo Abreu <abreu.rodrigo97@gmail.com>
2019-04-16 11:40:15 -0300

    Primeiro passo do DB

---
+++ atividade01/src/java/AppSecurityInitializer.java
@@ -0,0 +1,9 @@
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class AppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+  public AppSecurityInitializer() {
+      super(AppConfig.class);
+  }
+}

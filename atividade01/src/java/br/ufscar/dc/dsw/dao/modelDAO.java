d8dfb07eabfe83cfeffbbdaf9742bc0c16fb218b
Cassiano Maia <cassmala@gmail.com>
2019-04-25 15:26:28 -0300

    Refatorando o modelDAO

--- atividade01/src/java/br/ufscar/dc/dsw/dao/modelDAO.java
+++ atividade01/src/java/br/ufscar/dc/dsw/dao/modelDAO.java
@@ -5,6 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class modelDAO {
+    
+    public modelDAO() {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
     protected Connection getConnection() throws SQLException {
         return DriverManager.getConnection("jdbc:derby://localhost:1527/DBLocadora", "root", "root");
     }

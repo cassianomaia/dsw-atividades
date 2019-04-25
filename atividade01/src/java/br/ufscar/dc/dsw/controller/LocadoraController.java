abfc044d54f738dfe10823b6673b5c98730dd469
cassianomaia <cassmala@gmail.com>
2019-04-25 11:06:44 -0300

    Faltou um ;

--- atividade01/src/java/br/ufscar/dc/dsw/controller/LocadoraController.java
+++ atividade01/src/java/br/ufscar/dc/dsw/controller/LocadoraController.java
@@ -96,7 +96,7 @@ public class LocadoraController extends HttpServlet{
     private void atualize(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        Integer id = Integer.parseInt(request.getParameter("id"))
+        Integer id = Integer.parseInt(request.getParameter("id"));
         String nome = request.getParameter("nome");
         String email = request.getParameter("email");
         String senha = request.getParameter("senha");

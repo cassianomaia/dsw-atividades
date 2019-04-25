cf7647b0e2b3558bf4475555c0ba682545a6a174
cassianomaia <cassmala@gmail.com>
2019-04-25 09:37:03 -0300

    Adcionando o controller da locadora

---
+++ atividade01/src/java/br/ufscar/dc/dsw/controller/LocadoraController.java
@@ -0,0 +1,80 @@
+package br.ufscar.dc.dsw.controller;
+
+import br.ufscar.dc.dsw.dao.LocadoraDAO;
+import br.ufscar.dc.dsw.model.Locadora;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/locadora/*")
+public class LocadoraController extends HttpServlet{
+       private LocadoraDAO dao;
+       
+    @Override
+    public void init() {
+        dao = new LocadoraDAO();
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+        String action = request.getServletPath();
+
+        try {
+            switch (action) {
+                case "/cadastro":
+                    apresentaFormCadastro(request, response);
+                    break;
+                case "/insercao":
+                    insere(request, response);
+                    break;
+                case "/remocao":
+                    remove(request, response);
+                    break;
+                case "/edicao":
+                    apresentaFormEdicao(request, response);
+                    break;
+                case "/atualizacao":
+                    atualize(request, response);
+                    break;
+                default:
+                    lista(request, response);
+                    break;
+            }
+        } catch (RuntimeException | IOException | ServletException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    private void lista(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Locadora> listaLocadora = dao.getAll();
+        request.setAttribute("listaLocadora", listaLocadora);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("views/locadora/lista.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void insere(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String titulo = request.getParameter("titulo");
+        String autor = request.getParameter("autor");
+        Integer ano = Integer.parseInt(request.getParameter("ano"));
+        Float preco = Float.parseFloat(request.getParameter("preco"));
+
+        Livro livro = new Livro(titulo, autor, ano, preco);
+        dao.insert(livro);
+        response.sendRedirect("lista");
+    }
+
+}

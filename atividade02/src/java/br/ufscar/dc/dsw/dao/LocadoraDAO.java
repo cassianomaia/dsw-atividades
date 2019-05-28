ef0725dbb2a8eea6409b66e4a3c6222e6a093920
cassianomaia <cassmala@gmail.com>
2019-05-28 11:18:00 -0300

    Adcionando dao de locadora

--- atividade02/src/java/br/ufscar/dc/dsw/dao/LocadoraDAO.java
+++ atividade02/src/java/br/ufscar/dc/dsw/dao/LocadoraDAO.java
@@ -1,33 +1,59 @@
 package br.ufscar.dc.dsw.dao;
 
 import br.ufscar.dc.dsw.pojo.Locadora;
+import br.ufscar.dc.dsw.pojo.Usuario;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 public class LocadoraDAO extends GenericDAO<Locadora> {
 
     @Override
     Locadora get(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = this.getEntityManager();
+        Locadora locadora = em.find(Locadora.class, id);
+        em.close();
+        return locadora;
     }
 
     @Override
     List<Locadora> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = this.getEntityManager();
+        Query q = em.createQuery("select l from Locadora l", Locadora.class);
+        List<Locadora> locadoras = q.getResultList();
+        em.close();
+        return locadoras;
     }
 
     @Override
-    void save(Locadora t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    void save(Locadora locadora) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(locadora);
+        tx.commit();
+        em.close();
     }
 
     @Override
-    void update(Locadora t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    void update(Locadora locadora) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.merge(locadora);
+        tx.commit();
+        em.close();
     }
 
     @Override
-    void delete(Locadora t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    void delete(Locadora locadora) {
+        EntityManager em = this.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        locadora = em.getReference(Locadora.class, locadora.getId());
+        tx.begin();
+        em.remove(locadora);
+        tx.commit();
     }
 
 }

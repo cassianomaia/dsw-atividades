701d1e8930acbd9093adf2cd50065c3503a79e7b
Rodrigo Abreu <abreu.rodrigo97@gmail.com>
2019-05-27 23:37:22 -0300

    Alterações não funcionais, tentativa que transição de telas login -> admin -> novoCliente

---
+++ atividade02/src/java/formularios/novoCliente.java
@@ -0,0 +1,16 @@
+package formularios;
+
+import br.ufscar.dc.dsw.pojo.Usuario;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class novoCliente {
+
+//    private Usuario cliente;
+    public novoCliente() {
+//        cliente = new Usuario();
+    }
+
+}

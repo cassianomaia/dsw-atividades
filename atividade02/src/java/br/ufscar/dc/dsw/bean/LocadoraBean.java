3a12548aedef103a25ef36d5786d996a6df7efb7
Rodrigo Abreu <abreu.rodrigo97@gmail.com>
2019-06-02 22:00:00 -0300

    CRUD Locadora funcional

--- atividade02/src/java/br/ufscar/dc/dsw/bean/LocadoraBean.java
+++ atividade02/src/java/br/ufscar/dc/dsw/bean/LocadoraBean.java
@@ -5,14 +5,17 @@
  */
 package br.ufscar.dc.dsw.bean;
 
+import br.ufscar.dc.dsw.dao.ClienteDAO;
 import br.ufscar.dc.dsw.dao.LocadoraDAO;
 import br.ufscar.dc.dsw.dao.PapelDAO;
+import br.ufscar.dc.dsw.pojo.Cliente;
 import br.ufscar.dc.dsw.pojo.Locadora;
 import br.ufscar.dc.dsw.pojo.Papel;
 import java.sql.SQLException;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  *
@@ -22,15 +25,16 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class LocadoraBean {
 
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
     private Locadora locadora;
 
     public String lista() {
-        return "locadora/listaLocadoras.xhtml";
+        return "locadora/listaLocadoras.xhtml?faces-redirect=true";
     }
 
     public String cadastra() {
         locadora = new Locadora();
-        return "locadora/formulario.xhtml";
+        return "formulario.xhtml";
     }
 
     public String formulario() {
@@ -40,29 +44,30 @@ public class LocadoraBean {
     public String edita(Long id) {
         LocadoraDAO dao = new LocadoraDAO();
         locadora = dao.get(id);
-        return "form.xhtml";
+        return "formulario.xhtml";
     }
 
     public String salva() {
         LocadoraDAO dao = new LocadoraDAO();
-
-        Papel p1 = new PapelDAO().get(3L);
+        locadora.setSenha(encoder.encode(locadora.getSenha()));
+        long idpapel = 3;
+        Papel papellocadora = new PapelDAO().get(idpapel);
 
         if (locadora.getId() == null) {
+            locadora.setAtivo(true);
             dao.save(locadora);
-
-            locadora.getPapel().add(p1);
+            locadora.getPapel().add(papellocadora);
             dao.update(locadora);
         } else {
             dao.update(locadora);
         }
-        return "lista.xhtml";
+        return "listaLocadoras.xhtml";
     }
 
     public String delete(Locadora locadora) {
         LocadoraDAO dao = new LocadoraDAO();
         dao.delete(locadora);
-        return "index.xhtml";
+        return "listaLocadoras.xhtml";
     }
 
     public String volta() {
